f7124b29550a7e767f958a0fc5b81f961c40ef3f
AceWinspire <acevojinovic@gmail.com>
2019-02-08 02:37:48 +0100

    Adding Layout style and content

--- public/js/main.js
+++ public/js/main.js
@@ -72,3 +72,60 @@ $('.rerun-button').click(function(){
     $(document).on('click', '.owl-item>div', function() {
       $owl.trigger('to.owl.carousel', $(this).data( 'position' ) );
     });
+
+//menu show on scroll up
+  $(document).ready(function(){
+    console.log('Window Height is: ' + $(window).height());
+    console.log('Document Height is: ' + $(document).height());
+    var previousScroll = 0;
+    $(window).scroll(function(){
+      var currentScroll = $(this).scrollTop();
+      if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
+        if (currentScroll > previousScroll){
+          window.setTimeout(hideNav, 200);
+        } else {
+          window.setTimeout(showNav, 250);
+        }
+        previousScroll = currentScroll;
+      }
+    });
+    function hideNav() {
+      $("[data-nav-status='toggle']").removeClass("nav-show").addClass("nav-hide");
+    }
+    function showNav() {
+      $("[data-nav-status='toggle']").removeClass("nav-hide").addClass("nav-show");
+    }
+  });
+ 
+   
+  AOS.init();
+
+  //Embed social
+  (function (d, s, id) {
+    var js;
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://embedsocial.com/embedscript/ri.js";
+    d.getElementsByTagName("head")[0].appendChild(js);
+}(document, "script", "EmbedSocialReviewsScript"));
+
+//Instagram feed
+$(function(){
+    var accessToken = '273700138.f359f2c.f6d5dd3e37704cb8b976331d56706931';
+    $.getJSON('https://api.instagram.com/v1/users/self/media/recent/?access_token='+accessToken+'&callback=?',function (insta) {
+      $.each(insta.data,function (photos,src) {
+        if ( photos === 4 ) { return false; }
+        $('<a href="'+src.link+'" class="post">'+
+          '<div class="image" style="background-image:url('+src.images.standard_resolution.url+');"></div>'+
+          '<ul>'+
+          '<li><i class="fa fa-camera"></i> '+src.filter+'</li>'+
+          '<li><i class="fa fa-heart"></i> '+src.likes.count+'</li>'+
+          '<li><i class="fa fa-comment"></i> '+src.comments.count+'</li>'+
+          '</ul></a>').appendTo('#instafeed');
+      });
+    });
+  });
+  
